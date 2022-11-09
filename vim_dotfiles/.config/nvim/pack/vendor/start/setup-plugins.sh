4ce3973e26adb07955b9249127cce5aa891943b3
LovroBucar <lovro.bucar@gmail.com>
2022-11-09 18:14:44 +0100

    Fix .gitignore to exclude setup-plugins.sh

---
+++ vim_dotfiles/.config/nvim/pack/vendor/start/setup-plugins.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/preservim/nerdtree
+git clone https://github.com/bling/vim-bufferline
+git clone https://github.com/tpope/vim-commentary
+git clone https://github.com/airblade/vim-gitgutter
+git clone https://github.com/tpope/vim-surround
+git clone https://github.com/kana/vim-textobj-user
+git clone https://github.com/kana/vim-textobj-indent
